354bda31a59acbd76607cc15494cb2d3aa7b045f
Tóth Lajos <tothlajosg@gmail.com>
2015-03-02 18:47:12 +0100

    Working OpenGL, OpenCL skeleton

--- SPHFluidSimulation/SPHFluidSimulation/SPHFluidSimulation.cpp
+++ SPHFluidSimulation/SPHFluidSimulation/SPHFluidSimulation.cpp
@@ -2,10 +2,66 @@
 //
 
 #include "stdafx.h"
+#include <iostream>
+#include <cmath>
+#include <GL/glew.h>
+#include <GL/freeglut.h>
+#include <CL/cl.h>
 
+const int width = 600;
+const int height = 600;
 
-int _tmain(int argc, _TCHAR* argv[])
+#define PARTICLERADIUS 300
+
+void display()
 {
-	return 0;
+	glClearColor(0, 0, 1, 0);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	glPushMatrix();
+	glTranslatef(300, 300, 0);
+	glutSolidSphere(PARTICLERADIUS, 10, 10);
+	glPopMatrix();
+
+
+	glutSwapBuffers();
 }
+
+void idle()
+{
+	glutPostRedisplay();
+}
+
+void onInitialization() {
+	glMatrixMode(GL_PROJECTION);
+	glOrtho(0, 599, 0, 599, PARTICLERADIUS, -PARTICLERADIUS);
+}
+
+int main(int argc, char* argv[]){
+	glutInit(&argc, argv);
+	glutInitContextVersion(3, 0);
+	glutInitContextFlags(GLUT_CORE_PROFILE | GLUT_DEBUG);
+	glutInitDisplayMode(GLUT_RGBA | GLUT_DEPTH | GLUT_DOUBLE);
+	glutInitWindowSize(width, height);
+	glutCreateWindow("GPGPU 13. labor: Incompressible fluid simulation");
+
+
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	glMatrixMode(GL_PROJECTION);			
+	glLoadIdentity();
+
+	onInitialization();					
+
+	glutDisplayFunc(display);
+	glutIdleFunc(idle);
+
+	// OpenCL processing
+	//initSimulation();
+
+	glutMainLoop();
+
+	return(0);
+}
+
 
